245b973ed0ad937bf5f60592c87cff15c2266b0f
John Jackson <john@cask.co>
2016-11-09 19:23:03 -0800

    Data files from docs.cask.co New file is docs.cask.co/www/resources/scripts/redirect-page.js Modified file is docs.cask.co/www/404.html which has been changed to use it

---
+++ docs.cask.co/www/resources/scripts/redirect-page.js
@@ -0,0 +1,40 @@
+/*
+ * redirect-page.js
+ * ----------------------------
+ *
+ * JavaScript for redirecting to an appropriate lower-level 404 page
+ *
+ * :copyright: © Copyright 2016 Cask Data, Inc.
+ * :license: Apache License, Version 2.0
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ * 
+ */
+
+(function() {
+  var products = new Array( "cdap", "coopr", "tigon" );
+  var origin = window.location.origin;     //  "http://docs.cask.co"
+  var pathname = window.location.pathname; // "/cdap/3.6.0/en/test.html"
+  if (pathname[0] == '/') {
+    pathname = pathname.substr(1);
+  }
+  var pathparts = pathname.split( '/' );
+
+  if (products.indexOf(pathparts[0]) != -1 && pathparts.length > 1) {
+    var a = new Array(origin, pathparts[0], pathparts[1], 'en/404.html');
+    var url = a.join('/');  
+    if (url) {
+      window.location.replace(url);
+    }
+  }
+})();
